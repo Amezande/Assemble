0a464f28786489cf7778f5be8323bbbc0b872654
Axel Amacende <63935105+Amezande@users.noreply.github.com>
2023-02-10 12:20:45 -0600

    Update Suma.asm

--- Suma.asm
+++ Suma.asm
@@ -20,3 +20,29 @@ add al,bh
 
 sub al,ah; al minuendo, ah sustraendo, para todos los valores earitméticos restarle 30h para poder hacerlos decimal
 
+/*mov ah,01
+int 21h
+
+mov ah,02
+mov dl,"+"
+int 21h
+       
+mov ah,01
+int 21h
+
+
+mov al,3 
+mov ah,7
+mov bl,10d
+mov bh,37h
+
+
+add al,bh 
+add ah,al
+add bl,al
+add bl,5
+add bh,38
+
+
+sub al,ah*/
+
