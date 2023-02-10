528c1d08ac6396518579fdd787484567281cc9e1
Axel Amacende <63935105+Amezande@users.noreply.github.com>
2023-02-10 12:07:56 -0600

    Create Suma.asm

---
+++ Suma.asm
@@ -0,0 +1,21 @@
+mov ah,01
+int 21h
+
+mov ah,02
+mov dl,"+"
+int 21h
+       
+mov ah,01
+int 21h
+
+
+mov al,3 
+mov ah,7
+mov bl,10d
+mov bh,37h
+
+
+add al,bh
+
+
+sub al,ah; al minuendo, ah sustraendo
