c0c535d2d36dd16df389a02560f32c66f72313a3
Axel Amacende <63935105+Amezande@users.noreply.github.com>
2023-03-10 12:29:44 -0600

    Create Examen.asm

---
+++ Examen.asm
@@ -0,0 +1,351 @@
+;sumas parciales de 7 numeros todos los resultados al doble
+
+;mov ah,01; agregar
+;int 21h
+;sub al, 30h
+;add bl, al
+;mov ch, al 
+
+;mov ah, 02
+;mov dl, '*'
+;int 21h
+
+;mov al, bl
+
+;aam
+;mov bx, ax
+;add bx, 3030h
+;mov ah, 02
+;mov dl, bh
+;int 21h
+ 
+mov ah,01; agregar     ____________num 1
+int 21h
+sub al, 30h
+mov bl, al
+mov cl, bl
+
+mov ah,02 ; imprimir
+mov dl,"+"
+int 21h
+       
+mov ah,01; agregar
+int 21h
+sub al, 30h
+add bl, al
+mov ch, al
+
+mov ah, 02
+mov dl, '='
+int 21h
+
+mov al, bl
+
+aam
+mov bx, ax
+add bx, 3030h
+mov ah, 02
+mov dl, bh
+int 21h
+
+mov ah, 02
+mov dl, bl
+int 21h
+
+mov ah,02 ; imprimir
+mov dl,"*"
+int 21h 
+
+; 
+mov ah,02
+mov dl,32h
+int 21h   
+
+mov ah, 02
+mov dl, '='
+int 21h  
+
+;
+
+mov ah,01; agregar     ____________num 1
+int 21h
+sub al, 30h
+mov bl, al
+mov cl, bl
+
+mov ah,02 ; imprimir
+mov dl,"+"
+int 21h
+       
+mov ah,01; agregar
+int 21h
+sub al, 30h
+add bl, al
+mov ch, al
+
+mov ah, 02
+mov dl, '='
+int 21h
+
+mov al, bl
+
+aam
+mov bx, ax
+add bx, 3030h
+mov ah, 02
+mov dl, bh
+int 21h
+
+mov ah, 02
+mov dl, bl
+int 21h
+
+mov ah,02 ; imprimir
+mov dl,"*"
+int 21h 
+
+; 
+mov ah,02
+mov dl,32h
+int 21h   
+
+mov ah, 02
+mov dl, '='
+int 21h  
+
+;
+mov ah,01; agregar     ____________num 1
+int 21h
+sub al, 30h
+mov bl, al
+mov cl, bl
+
+mov ah,02 ; imprimir
+mov dl,"+"
+int 21h
+       
+mov ah,01; agregar
+int 21h
+sub al, 30h
+add bl, al
+mov ch, al
+
+mov ah, 02
+mov dl, '='
+int 21h
+
+mov al, bl
+
+aam
+mov bx, ax
+add bx, 3030h
+mov ah, 02
+mov dl, bh
+int 21h
+
+mov ah, 02
+mov dl, bl
+int 21h
+
+mov ah,02 ; imprimir
+mov dl,"*"
+int 21h 
+
+; 
+mov ah,02
+mov dl,32h
+int 21h   
+
+mov ah, 02
+mov dl, '='
+int 21h  
+
+;
+mov ah,01; agregar     ____________num 1
+int 21h
+sub al, 30h
+mov bl, al
+mov cl, bl
+
+mov ah,02 ; imprimir
+mov dl,"+"
+int 21h
+       
+mov ah,01; agregar
+int 21h
+sub al, 30h
+add bl, al
+mov ch, al
+
+mov ah, 02
+mov dl, '='
+int 21h
+
+mov al, bl
+
+aam
+mov bx, ax
+add bx, 3030h
+mov ah, 02
+mov dl, bh
+int 21h
+
+mov ah, 02
+mov dl, bl
+int 21h
+
+mov ah,02 ; imprimir
+mov dl,"*"
+int 21h 
+
+; 
+mov ah,02
+mov dl,32h
+int 21h   
+
+mov ah, 02
+mov dl, '='
+int 21h  
+
+;
+mov ah,01; agregar     ____________num 1
+int 21h
+sub al, 30h
+mov bl, al
+mov cl, bl
+
+mov ah,02 ; imprimir
+mov dl,"+"
+int 21h
+       
+mov ah,01; agregar
+int 21h
+sub al, 30h
+add bl, al
+mov ch, al
+
+mov ah, 02
+mov dl, '='
+int 21h
+
+mov al, bl
+
+aam
+mov bx, ax
+add bx, 3030h
+mov ah, 02
+mov dl, bh
+int 21h
+
+mov ah, 02
+mov dl, bl
+int 21h
+
+mov ah,02 ; imprimir
+mov dl,"*"
+int 21h 
+
+; 
+mov ah,02
+mov dl,32h
+int 21h   
+
+mov ah, 02
+mov dl, '='
+int 21h  
+
+;
+mov ah,01; agregar     ____________num 1
+int 21h
+sub al, 30h
+mov bl, al
+mov cl, bl
+
+mov ah,02 ; imprimir
+mov dl,"+"
+int 21h
+       
+mov ah,01; agregar
+int 21h
+sub al, 30h
+add bl, al
+mov ch, al
+
+mov ah, 02
+mov dl, '='
+int 21h
+
+mov al, bl
+
+aam
+mov bx, ax
+add bx, 3030h
+mov ah, 02
+mov dl, bh
+int 21h
+
+mov ah, 02
+mov dl, bl
+int 21h
+
+mov ah,02 ; imprimir
+mov dl,"*"
+int 21h 
+
+; 
+mov ah,02
+mov dl,32h
+int 21h   
+
+mov ah, 02
+mov dl, '='
+int 21h  
+
+; 
+mov ah,01; agregar     ____________num 1
+int 21h
+sub al, 30h
+mov bl, al
+mov cl, bl
+
+mov ah,02 ; imprimir
+mov dl,"+"
+int 21h
+       
+mov ah,01; agregar
+int 21h
+sub al, 30h
+add bl, al
+mov ch, al
+
+mov ah, 02
+mov dl, '='
+int 21h
+
+mov al, bl
+
+aam
+mov bx, ax
+add bx, 3030h
+mov ah, 02
+mov dl, bh
+int 21h
+
+mov ah, 02
+mov dl, bl
+int 21h
+
+mov ah,02 ; imprimir
+mov dl,"*"
+int 21h 
+
+; 
+mov ah,02
+mov dl,32h
+int 21h   
+
+mov ah, 02
+mov dl, '='
+int 21h  
+
+;
