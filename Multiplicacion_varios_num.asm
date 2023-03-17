c614d1d81d363c371b037060352e4b206e6c2d40
Axel Amacende <63935105+Amezande@users.noreply.github.com>
2023-03-17 12:30:40 -0600

    Create Multiplicacion_varios_num.asm

---
+++ Multiplicacion_varios_num.asm
@@ -0,0 +1,27 @@
+;____________________                
+; 2022350413
+
+mov al, 2
+mov bl, 2
+mul bl
+
+mov bl,2
+mul bl
+
+mov bl,3
+mul bl 
+
+mov bl,5
+mul bl        
+
+mov bl,4
+mul bl 
+
+mov bl,1     ; lo pasamos a bx para que pueda seguir multiplicando, para registros de 3 numeros
+mul bx  
+
+mov bl,3
+mul bx       
+; Tareaa calcular el factorial de un numero
+
+
